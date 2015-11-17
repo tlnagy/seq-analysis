c1010fe1108e1c906b17c9058b33bd43ae8b6e67
Tamas Nagy <tamas@tamasnagy.com>
2015-11-16 20:10:44 -0800

    process all groups simultaneously

--- utils.py
+++ utils.py
@@ -2,21 +2,24 @@ import pandas as pd
 import numpy as np
 
 
-def load_gen_times(experimental_info_csv_path):
+def load_gen_times(df, experimental_info_csv_path):
     """
-    Loads generation times from the experimental info csv on the Pubs website
+    Loads generation times from the experimental info csv on the Pubs website and
+    adds it to the dataframe
 
+    :param df:
     :param experimental_info_csv_path:
     :return:
     """
-    primers = pd.read_csv(experimental_info_csv_path)
+    primers = pd.read_csv("seq-analysis/truseq_primers.csv")
     primers["exp"] = primers["Sample"].str.replace("Day", "d").str.replace(" T=", "t").str.split(" ").str.get(1)
     primers["group"] = primers["Sample"].str.split(" ").str.get(0)
     primers = primers[["group", "exp", "Generations"]]
     primers = primers[~pd.isnull(primers["group"])].fillna(0).replace("-", 0)
     primers[["days", "timepoints"]] = primers["exp"].str.replace(r"t", "_t").str.split("_", expand=True)
     primers = primers.drop("exp", axis=1)
-    gen_times = {}
-    for group, group_data in primers.groupby("group"):
-        gen_times.update({group: {day: data["Generations"].astype(np.float).tolist() for day, data in group_data.groupby("days")}})
-    return gen_times
+    primers = primers.set_index(["group", "days", "timepoints"])
+    primers = primers.astype(np.float)
+
+    df = df.stack("timepoints").reset_index().set_index(["group", "days", "timepoints"]).join(primers)
+    return df.set_index(["barcodes", "codons", "amino acids", "positions"], append=True).unstack("timepoints")
