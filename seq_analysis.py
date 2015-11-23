Added code for weighted standard deviation and fixed a bug where the columns did not match their names.

seq_analysis.py
@@ -79,7 +79,7 @@ def process_data(hdf5_datastorepath, allele_pkl_path = None, experimental_info_c
 
     all_tp = rel_wt_gen_times[np.all(pd.notnull(rel_wt_gen_times["rel_wt"]), axis=1)]
 
-    slopes = _groupby_parallel(all_tp.groupby(level=["group", "days", "amino acids"]), _linregress_df)
+    slopes = groupby_parallel(all_tp.groupby(level=["group", "days", "amino acids"]), _linregress_df)
 
     # handle barcodes that only show up in two timepoints
     two_tp_only = rel_wt_gen_times[pd.isnull(rel_wt_gen_times["rel_wt"]).sum(axis=1) == 1]
@@ -98,7 +98,9 @@ def process_data(hdf5_datastorepath, allele_pkl_path = None, experimental_info_c
     normed_df = pd.DataFrame(normed_df.values, index=normed_df.index, columns=new_cols)
     slopes = pd.concat([slopes, normed_df], axis=1)
 
-    return slopes, calc_fitness_by_codon(slopes), calc_fitness_by_aa(slopes)
+    codon_fitness = calc_fitness_by_codon(slopes)
+    aa_fitness = calc_fitness_by_aa(slopes)
+    return slopes, codon_fitness, aa_fitness
 
 
 def calc_fitness_by_codon(slopes):
@@ -106,12 +108,27 @@ def calc_fitness_by_codon(slopes):
     Calculates fitness on a per-codon basis.
     :param slopes: A dataframe of per-barcode fitness.
     '''
-    extra_col_names, extra_funcs = _init_weighting()
+
+    #slopes = slopes.copy()
+    #slopes = slopes[slopes[('counts', "t0")] > 0] # restrict to those with counts
+
     codons_weighted = slopes.groupby(level=["group", "days", "codons", "positions"]).apply(_weighted_avg)
     codons_weighted.name = "weighted mean slope"
-    codons_extra = slopes.groupby(level=["group", "days", "codons", "positions"]).agg(extra_funcs)
-    codons_extra.columns = extra_col_names
-    return pd.concat([codons_weighted, codons_extra], axis=1)
+
+    # Don't use a dictionary because the order is not preserved
+    counts = slopes.groupby(level=["group", "days", "codons", "positions"]).apply(lambda x: x[("counts", "t0")].sum())
+    counts.name = 'sum of t0 reads'
+    n_barcodes = slopes.groupby(level=["group", "days", "codons", "positions"]).apply(lambda x: len(x))
+    n_barcodes.name = '# unique barcodes'
+    stds = slopes.groupby(level=["group", "days", "codons", "positions"]).apply(lambda x: x[("fitness", "slope")].std())
+    stds.name = 'stddev of slope'
+
+    combined = pd.concat([codons_weighted, counts, n_barcodes, stds], axis=1)
+
+    weighted_stddev = combined.groupby(level=["group", "days", "codons", "positions"]).apply(_weighted_std)
+    weighted_stddev.name = "weighted stddev"
+
+    return pd.concat([combined, weighted_stddev], axis=1)
 
 
 def calc_fitness_by_aa(slopes):
@@ -119,12 +136,24 @@ def calc_fitness_by_aa(slopes):
     Calculates fitness on a per-amino acid basis.
     :param slopes: A dataframe of per-barcode fitness.
     '''
-    extra_col_names, extra_funcs = _init_weighting()
+
     aa_weighted = slopes.groupby(level=["group", "days", "amino acids", "positions"]).apply(_weighted_avg)
     aa_weighted.name = "weighted mean slope"
-    aa_extra = slopes.groupby(level=["group", "days", "amino acids", "positions"]).agg(extra_funcs)
-    aa_extra.columns = extra_col_names
-    return pd.concat([aa_weighted, aa_extra], axis=1)
+
+    # Don't use a dictionary because the order is not preserved
+    counts = slopes.groupby(level=["group", "days", "amino acids", "positions"]).apply(lambda x: x[("counts", "t0")].sum())
+    counts.name = 'sum of t0 reads'
+    n_barcodes = slopes.groupby(level=["group", "days", "amino acids", "positions"]).apply(lambda x: len(x))
+    n_barcodes.name = '# unique barcodes'
+    stds = slopes.groupby(level=["group", "days", "amino acids", "positions"]).apply(lambda x: x[("fitness", "slope")].std())
+    stds.name = 'stddev of slope'
+
+    combined = pd.concat([aa_weighted, counts, n_barcodes, stds], axis=1)
+
+    weighted_stddev = slopes.groupby(level=["group", "days", "amino acids", "positions"]).apply(_weighted_std)
+    weighted_stddev.name = "weighted stddev"
+
+    return pd.concat([combined, weighted_stddev], axis=1)
 
 
 def _init_weighting():
@@ -134,7 +163,18 @@ def _init_weighting():
 
 
 def _weighted_avg(x):
-    return (x[("fitness", "slope")]*np.log(x[("counts", "t0")])/np.log(x[("counts", "t0")]).sum()).sum()
+    return (x[("fitness", "slope")] * np.log(x[("counts", "t0")]) / np.log(x[("counts", "t0")]).sum()).sum()
+
+
+def _weighted_std(x):
+    m = len(x)
+    weight_sum = np.log(x[("counts", "t0")]).sum()
+    if m < 2 or weight_sum == 0:
+        return float('NaN')
+    weighted_mean_slopes = x[("fitness", "slope")] * np.log(x[("counts", "t0")]) / np.log(x[("counts", "t0")]).sum()
+    dist_from_mean = weighted_mean_slopes - np.full(m, weighted_mean_slopes.mean())
+    numerator = (np.log(x[('counts', "t0")]) * dist_from_mean ** 2).sum()
+    return np.sqrt(numerator / weight_sum)
 
 
 def _linregress_wrapper(xs, ys):
@@ -150,7 +190,7 @@ def _linregress_df(args):
     return result
 
 
-def _groupby_parallel(groupby_df, func):
+def groupby_parallel(groupby_df, func):
     start = timer()
     num_cpus = cpu_count()
     print("\nUsing {} cpus in parallel...\n\nPercent complete: ".format(num_cpus), end="")