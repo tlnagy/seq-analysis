932583a286e78d803b97c00a1f8e65de5e838c5a
Douglas Myers-Turnbull <dmyersturnbull@gmail.com>
2015-11-22 17:27:10 -0800

    Allow comparisons between groups as well as days.

--- case_control.py
+++ case_control.py
@@ -24,7 +24,7 @@ def calc_pvals(cc, k = 800):
     return cc
 
 
-def calc_diff(fitnesses, group, control_day = 'd1', case_day='d2', n_sims = 800):
+def calc_diff(fitnesses, case_group, control_group="Control", control_day='d1', case_day='d1', n_sims=800, by_codons=False):
     '''
     Calculates the difference between case and control fitness values with p-values calculated by permuting the data across both.
     :param fitnesses: Pandas dataframe from seq_analysis of fitness values at either the codon or amino acid level
@@ -34,23 +34,26 @@ def calc_diff(fitnesses, group, control_day = 'd1', case_day='d2', n_sims = 800)
     :n_simulations: The number of iterations to use in the permutation test; set to 0 to disable
     :return: A Pandas dataframe with columns [slope_control, slope_case, slope_diff, std_control, std_case, p_val]
     '''
-    if group is not None:
-        days = fitnesses[fitnesses.index.get_level_values(level = 'group') == group]
-    else:
-        days = fitnesses
-    days = days.groupby(level = 'days')
-    control = days.get_group(control_day)
-    case = days.get_group(case_day)
-    case.columns = case.columns.get_level_values(1)
-    control.columns = control.columns.get_level_values(1)
+    control = fitnesses
+    case = fitnesses
+    if control_group is not None:
+        control = fitnesses[fitnesses.index.get_level_values(level = 'group') == control_group]
+    if case_group is not None:
+        case = fitnesses[fitnesses.index.get_level_values(level = 'group') == case_group]
+    if control_day is not None:
+        control = control[fitnesses.index.get_level_values(level = 'days') == control_day]
+    if case_day is not None:
+        case = control[fitnesses.index.get_level_values(level = 'days') == case_day]
+    case.columns = case.columns.get_level_values(1) # flatten
+    control.columns = control.columns.get_level_values(1) # flatten
     control = control.reset_index()
     case = case.reset_index()
-    wanted_cols = [c for c in ['codons', 'positions', 'amino acids', '# unique barcodes', 'weighted mean slope', 'stddev of slope', 'sum of t0 reads'] if c in case.columns]
-    wanted_indices = [c for c in ['codons', 'positions', 'amino acids'] if c in case.columns]
+    wanted_cols = [c for c in ['group', 'days', 'positions', '# unique barcodes', 'weighted mean slope', 'stddev of slope', 'weighted stddev', 'sum of t0 reads', 'codons' if by_codons else 'amino acids'] if c in case.columns]
+    wanted_indices = [c for c in ['group', 'days', 'positions', 'codons' if by_codons else 'amino acids'] if c in case.columns]
     case = case[wanted_cols]
     control = control[wanted_cols]
     merged = pd.merge(control, case, how='inner', on=wanted_indices, suffixes=['_control', '_case'])
-    merged.set_index(['codons', 'amino acids', 'positions'], inplace=True)
+    merged.set_index(wanted_indices, inplace=True)
     merged['slope_diff'] = merged['slope_case'] - merged['slope_control']
     if n_sims > 0:
         calc_pvals(merged, n_sims)
