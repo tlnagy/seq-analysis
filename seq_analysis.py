Simplified code and fixed a bug in by-codon fitness calculation.

seq_analysis.py
@@ -101,75 +101,43 @@ def process_data(hdf5_datastorepath, allele_pkl_path = None, experimental_info_c
     normed_df = pd.DataFrame(normed_df.values, index=normed_df.index, columns=new_cols)
     slopes = pd.concat([slopes, normed_df], axis=1)
 
-    codon_fitness = calc_fitness_by_codon(slopes)
-    aa_fitness = calc_fitness_by_aa(slopes)
+    codon_fitness = calc_fitness_by(slopes, by_codons=True)
+    aa_fitness = calc_fitness_by(slopes, by_codons=False)
     return slopes, codon_fitness, aa_fitness
 
 
-def calc_fitness_by_codon(slopes):
-    '''
-    Calculates fitness on a per-codon basis.
-    :param slopes: A dataframe of per-barcode fitness.
-    '''
-
-    #slopes = slopes.copy()
-    #slopes = slopes[slopes[('counts', "t0")] > 0] # restrict to those with counts
-
-    codons_weighted = slopes.groupby(level=["group", "days", "codons", "positions"]).apply(_weighted_avg)
-    codons_weighted.name = "weighted mean slope"
-
-    # Don't use a dictionary because the order is not preserved
-    counts = slopes.groupby(level=["group", "days", "codons", "positions"]).apply(lambda x: x[("counts", "t0")].sum())
-    counts.name = 'sum of t0 reads'
-    n_barcodes = slopes.groupby(level=["group", "days", "codons", "positions"]).apply(lambda x: len(x))
-    n_barcodes.name = '# unique barcodes'
-    stds = slopes.groupby(level=["group", "days", "codons", "positions"]).apply(lambda x: x[("fitness", "slope")].std())
-    stds.name = 'stddev of slope'
-
-    combined = pd.concat([codons_weighted, counts, n_barcodes, stds], axis=1)
-
-    weighted_stddev = combined.groupby(level=["group", "days", "codons", "positions"]).apply(_weighted_std)
-    weighted_stddev.name = "weighted stddev"
-
-    return pd.concat([combined, weighted_stddev], axis=1)
-
-
-def calc_fitness_by_aa(slopes):
+def calc_fitness_by(slopes, by_codons=False):
     '''
     Calculates fitness on a per-amino acid basis.
     :param slopes: A dataframe of per-barcode fitness.
     '''
 
-    aa_weighted = slopes.groupby(level=["group", "days", "amino acids", "positions"]).apply(_weighted_avg)
-    aa_weighted.name = "weighted mean slope"
+    group_on = ["group", "days", "codons" if by_codons else "amino acids", "positions"]
+
+    weighted = slopes.groupby(level=group_on).apply(weighted_avg)
+    weighted.name = "weighted mean slope"
 
     # Don't use a dictionary because the order is not preserved
-    counts = slopes.groupby(level=["group", "days", "amino acids", "positions"]).apply(lambda x: x[("counts", "t0")].sum())
+    counts = slopes.groupby(level=group_on).apply(lambda x: x[("counts", "t0")].sum())
     counts.name = 'sum of t0 reads'
-    n_barcodes = slopes.groupby(level=["group", "days", "amino acids", "positions"]).apply(lambda x: len(x))
+    n_barcodes = slopes.groupby(level=group_on).apply(lambda x: len(x))
     n_barcodes.name = '# unique barcodes'
-    stds = slopes.groupby(level=["group", "days", "amino acids", "positions"]).apply(lambda x: x[("fitness", "slope")].std())
+    stds = slopes.groupby(level=group_on).apply(lambda x: x[("fitness", "slope")].std())
     stds.name = 'stddev of slope'
 
-    combined = pd.concat([aa_weighted, counts, n_barcodes, stds], axis=1)
+    combined = pd.concat([weighted, counts, n_barcodes, stds], axis=1)
 
-    weighted_stddev = slopes.groupby(level=["group", "days", "amino acids", "positions"]).apply(_weighted_std)
+    weighted_stddev = slopes.groupby(level=group_on).apply(weighted_std)
     weighted_stddev.name = "weighted stddev"
 
     return pd.concat([combined, weighted_stddev], axis=1)
 
 
-def _init_weighting():
-    extra_col_names = ["# unique barcodes", "stddev of slope", "sum of t0 reads"]
-    extra_funcs = {("fitness", "slope"):[len, np.std], ("counts", "t0"):np.sum}
-    return extra_col_names, extra_funcs
-
-
-def _weighted_avg(x):
+def weighted_avg(x):
     return (x[("fitness", "slope")] * np.log(x[("counts", "t0")]) / np.log(x[("counts", "t0")]).sum()).sum()
 
 
-def _weighted_std(x):
+def weighted_std(x):
     m = len(x)
     weight_sum = np.log(x[("counts", "t0")]).sum()
     if m < 2 or weight_sum == 0: